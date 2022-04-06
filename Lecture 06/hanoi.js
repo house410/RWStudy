e8f6e1cc6d02176da475ccc206381053b2d85518
원기 김 <hose410@naver.com>
2022-04-06 19:18:38 +0900

    [wk] 반응형웹6주차과제

---
+++ Lecture 06/hanoi.js
@@ -0,0 +1,9 @@
+function Init() {
+    var b = document.getElementsByClassName('b');
+b[0].style.display = 'block';
+b[1].style.display = 'block';
+b[2].style.display = 'block';
+b[3].style.display = 'block';
+b[4].style.display = 'block';
+b[5].style.display = 'block';
+  }
